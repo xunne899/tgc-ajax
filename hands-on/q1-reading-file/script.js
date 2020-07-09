ebe1f87d3708efdc1bf2684e0466ea4ef72fcac4
kunxin-chor <chorkunxin@yahoo.com>
2020-07-09 06:02:35 +0000

    Example 3

--- hands-on/q1-reading-file/script.js
+++ hands-on/q1-reading-file/script.js
@@ -1,4 +1,17 @@
 let b = document.querySelector('#load-btn');
-b.addEventListener('click', function(){
+// b.addEventListener('click', function(){
+//     axios.get('contact.txt').then(function(response){
+//         let content = response.data;
+W//         // let select the div
+//         let contentDiv = document.querySelector("#content");
+//         contentDiv.innerHTML = content;
+//     })
+// })
 
+// AWAIT/ASYNC
+b.addEventListener('click', async function(){
+    let response  = await axios.get('contact.txt');
+    let contentDiv = document.querySelector("#content");
+    contentDiv.innerHTML = response.data;
 })
+
