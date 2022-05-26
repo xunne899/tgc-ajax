d1bb6db736fa892c59df8b6d693e4ca8f4aaa1aa
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-05-26 08:04:40 +0000

    add new example on qn4

--- hands-on/q4-simple-json-example/script.js
+++ hands-on/q4-simple-json-example/script.js
@@ -4,10 +4,13 @@ btn.addEventListener('click', async function(){
 
 
 
+
+        // document.querySelector('#artwork').innerHTML += `<h1>${movie.title}</h1>`;
+        // document.querySelector('#artwork').innerHTML += `<img class="${movie.class}" src="${movie.image_url}">`
+
 let response = await axios.get('artwork.json');
-    let movie = response.data;
-    // document.querySelector('#artwork').innerHTML += `<h1>${movie.title}</h1>`;
-    // document.querySelector('#artwork').innerHTML += `<img class="${movie.class}" src="${movie.image_url}">`
+let movie = response.data;
+
 let title = document.querySelector('#title')
     title.innerHTML = movie.title;
 let image =  document.querySelector('#image')
@@ -15,3 +18,22 @@ let image =  document.querySelector('#image')
 
 
 })
+
+
+
+document.querySelector('#load-image-btn')
+    .addEventListener("click", async ()=>{
+       let response =  await axios.get('artwork.json');
+       console.log(response.data);
+
+        let h2Element = document.createElement('h2');
+        h2Element.innerHTML = response.data.title;
+
+        let imageElement = document.createElement('img')
+        imageElement.src = response.data.image_url;
+// for new element created to be show need to appendchild
+        let artWorkDiv = document.querySelector('#artwork');
+        artWorkDiv.appendChild(h2Element);
+        artWorkDiv.appendChild(imageElement);
+
+    })
